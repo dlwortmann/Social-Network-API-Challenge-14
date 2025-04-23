114bb1196e4bde74b91c970aead0f6db76d9b2ad
dlwortmann <dannywortmann@gmail.com>
2025-04-23 12:41:04 -0500

    update seeding information

--- src/utils/Date.ts
+++
@@ -1,7 +0,0 @@
-// import moment from 'moment';
-
-// const formatDate = (date) => {
-//     return moment(date).format("MM/DD/YYYY, h:mm:ss")
-// }
-
-// export default formatDate;
--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -1,35 +1,15 @@
 import connection from "../config/connection.js"
 import { User } from '../models/index.js'
-import getRandomName from './data.js'
 
-console.log(getRandomName())
 connection.on('error', (err) => err)
 
 connection.once('open', async () => {
-    let thoughtCheck = await connection.db?.listCollections({ name: 'thoughts' }).toArray();
-    if (thoughtCheck?.length) {
-        await connection.dropCollection('thoughts')
-    }
+    console.log('connected')
+    await User.deleteMany({})
+    await User.collection.insertMany(users)
 
-    let userCheck = await connection.db?.listCollections({ name: 'users' }).toArray()
-    if (userCheck?.length) {
-        await connection.dropCollection('users')
-    }
+    console.table(users)
+    console.info('Seeding completed!')
 
-    const users = []
-
-    for (let i = 0; i < 20; i++) {
-        const fullName = getRandomName()
-        const first = fullName.split(' ')[0]
-        const last = fullName.split(' ')[1]
-
-        users.push({
-            first,
-            last,
-        })
-    }
-
-    await User.insertMany(users)
-    console.log(users)
     process.exit(0)
 })
